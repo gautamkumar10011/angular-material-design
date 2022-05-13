ba835d0f87dd41a8ec225a78ac9b5002403695d9
Gautam Kumar <gautam.kumar.10011@gmail.com>
2022-05-13 14:19:35 +0530

    progress_spinner

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,14 +5,21 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatBadgeModule } from '@angular/material/badge';
-
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatRadioModule } from '@angular/material/radio';
 const MaterialComponents = [
   MatButtonModule,
   MatButtonToggleModule,
   MatIconModule,
   MatDividerModule,
   MatMenuModule,
-  MatBadgeModule
+  MatBadgeModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  MatCardModule,
+  MatRadioModule
 ]
 
 @NgModule({
